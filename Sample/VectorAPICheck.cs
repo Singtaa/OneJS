c1d5fce4ecdaceedf2f8106d03238ae9adfadeda
David <st0724@gmail.com>
2022-12-22 23:50:15 -0800

    Explicit Vector API Check for SampleScene

---
+++ Sample/VectorAPICheck.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+
+namespace OneJS.Misc {
+    public class VectorAPICheck : MonoBehaviour {
+#if !UNITY_2022_1_OR_NEWER
+        void Awake() {
+            Debug.LogError(
+                "This sample uses the UI Toolkit Vector API which requires Unity 2022.1 or later. You are currently using " +
+                Application.unityVersion);
+        }
+#endif
+    }
+}
+#endif
