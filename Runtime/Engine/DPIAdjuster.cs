5030f3bc8150d9c90a62388fd3f5e0b038fa70b9
David <st0724@gmail.com>
2023-02-25 12:32:10 -0800

    Tweaks to DPIAdjuster

--- Runtime/Engine/DPIAdjuster.cs
+++ Runtime/Engine/DPIAdjuster.cs
@@ -1,32 +1,41 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
 namespace OneJS.Engine {
     [RequireComponent(typeof(UIDocument))]
     public class DPIAdjuster : MonoBehaviour {
-        float currentDPI;
-        string currentReolutionStr;
-        UIDocument uiDocument;
-        PanelSettings panelSettings;
+        [Tooltip("Automatically set scale based on DPI")]
+        [SerializeField] bool _autoSetUIScale;
+
+        [Tooltip("Default UI scale for Panel Settings")]
+        [SerializeField] float _defaultUIScale = 1f;
+
+        float _currentDPI;
+        string _currentReolutionStr;
+        UIDocument _uiDocument;
+        PanelSettings _panelSettings;
 
         void Awake() {
-            uiDocument = GetComponent<UIDocument>();
-            panelSettings = uiDocument.panelSettings;
-            Set();
+            _uiDocument = GetComponent<UIDocument>();
+            _panelSettings = _uiDocument.panelSettings;
+            _panelSettings.scale = _defaultUIScale;
+            if (_autoSetUIScale)
+                SetScale();
         }
 
         void Update() {
-            if (Math.Abs(currentDPI - Screen.dpi) > 0.1f &&
-                currentReolutionStr != Screen.currentResolution.ToString()) {
-                Set();
+            if (_autoSetUIScale && Math.Abs(_currentDPI - Screen.dpi) > 0.1f &&
+                _currentReolutionStr != Screen.currentResolution.ToString()) {
+                SetScale();
             }
         }
 
-        void Set() {
-            currentDPI = Screen.dpi;
-            currentReolutionStr = Screen.currentResolution.ToString();
-            panelSettings.scale = currentDPI > 130 ? 2f : 1f;
+        void SetScale() {
+            _currentDPI = Screen.dpi;
+            _currentReolutionStr = Screen.currentResolution.ToString();
+            _panelSettings.scale = _currentDPI > 130 ? 2f : 1f;
         }
     }
 }
