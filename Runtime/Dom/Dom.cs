9a0f460e445ee57dc0ca8887ed76f63b6445fcbb
David <st0724@gmail.com>
2023-04-03 20:37:21 -0700

    Improved addEventListener and removeEventListener to allow multiple callbacks on the same event

--- Runtime/Dom/Dom.cs
+++ Runtime/Dom/Dom.cs
@@ -10,6 +10,11 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace OneJS.Dom {
+    public class RegisteredCallbackHolder {
+        public EventCallback<EventBase> callback;
+        public JsValue jsValue;
+    }
+
     public class Dom {
         public Document document => _document;
 
@@ -69,8 +74,8 @@ namespace OneJS.Dom {
         object __children;
         Dictionary<string, JsValue> __listeners = new Dictionary<string, JsValue>();
 
-        Dictionary<string, EventCallback<EventBase>> _registeredCallbacks =
-            new Dictionary<string, EventCallback<EventBase>>();
+        Dictionary<string, List<RegisteredCallbackHolder>> _registeredCallbacks =
+            new Dictionary<string, List<RegisteredCallbackHolder>>();
 
         static Dictionary<string, RegisterCallbackDelegate> _eventCache =
             new Dictionary<string, RegisterCallbackDelegate>();
@@ -129,6 +134,7 @@ namespace OneJS.Dom {
                 engine.Call(jsval, thisDom, new[] { JsValue.FromObject(engine, e) });
             });
             var isValueChanged = name == "ValueChanged";
+            // Debug.Log("addEventListener " + name + " on " + _ve.name + " " + isValueChanged);
 
             if (!isValueChanged && _eventCache.ContainsKey(name)) {
                 _eventCache[name](_ve, callback, TrickleDown.NoTrickleDown);
@@ -149,28 +155,46 @@ namespace OneJS.Dom {
                     mi = mi.MakeGenericMethod(eventType);
                     // mi.Invoke(null, new object[] { _ve, callback });
                     var del = (RegisterCallbackDelegate)Delegate.CreateDelegate(typeof(RegisterCallbackDelegate), mi);
-                    if (!isValueChanged)
+                    if (!isValueChanged && !_eventCache.ContainsKey(name))
                         _eventCache.Add(name, del);
                     del(_ve, callback, TrickleDown.NoTrickleDown);
                     // Debug.Log("Registered " + name + " on " + _ve.name);
                 }
             }
 
-            _registeredCallbacks.Add(name, callback);
+            var callbackHolder = new RegisteredCallbackHolder() { callback = callback, jsValue = jsval };
+            if (_registeredCallbacks.ContainsKey(name))
+                _registeredCallbacks[name].Add(callbackHolder);
+            else
+                _registeredCallbacks.Add(name, new List<RegisteredCallbackHolder> { callbackHolder });
+
             // Debug.Log($"{name} {(DateTime.Now - t).TotalMilliseconds}ms");
         }
 
+        /// <summary>
+        /// Note that this method will remove ALL callbacks registered for the given event name.
+        /// </summary>
         public void removeEventListener(string name, JsValue jsval, bool useCapture = false) {
-            var callback = _registeredCallbacks[name];
+            if (!_registeredCallbacks.ContainsKey(name))
+                return;
+            var callbackHolders = _registeredCallbacks[name];
             var eventType = typeof(VisualElement).Assembly.GetType($"UnityEngine.UIElements.{name}Event");
             if (eventType != null) {
                 var flags = BindingFlags.Public | BindingFlags.Instance;
                 var mi = _ve.GetType().GetMethods(flags)
                     .Where(m => m.Name == "UnregisterCallback" && m.GetGenericArguments().Length == 1).First();
                 mi = mi.MakeGenericMethod(eventType);
-                mi.Invoke(_ve, new object[] { callback, null });
+                for (var i = 0; i < callbackHolders.Count; i++) {
+                    if (callbackHolders[i].jsValue == jsval) {
+                        mi.Invoke(_ve, new object[] { callbackHolders[i].callback, null });
+                        callbackHolders.RemoveAt(i);
+                        i--;
+                    }
+                }
+                if (callbackHolders.Count == 0) {
+                    _registeredCallbacks.Remove(name);
+                }
             }
-            _registeredCallbacks.Remove(name);
         }
 
         public void appendChild(Dom node) {
