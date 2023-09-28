c9a1f6882423a00b52c52ae3ada97bb2906de781
Sam Parton <sam@idevelopthings.com>
2023-09-28 13:47:48 +0100

    TSDefConverter Additions: - Extract options into their own class - Add extra options:   - Include Declare(adds the `declare module "..." { ... }` wrapper   - ExtractBaseDefinitions(also generates definitions for base classes/interfaces)   - ExcludeUnityBaseTypes(prevents generating types in `UnityEngine` namespace) - If a namespace is passed which is not directly a type, we'll assume it's a "root" and load all types inside of the ns, then ask if we want to generate all defs for these. Its good and bad.

--- Runtime/Utils/AssemblyFinder.cs
+++ Runtime/Utils/AssemblyFinder.cs
@@ -1,24 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
 namespace OneJS.Utils {
     public class AssemblyFinder {
         static Assembly[] _assemblies;
+        static Assembly[] Assemblies => _assemblies ??= AppDomain.CurrentDomain.GetAssemblies();
 
         /// <summary>
         /// Can be slow
         /// </summary>
         public static Type FindType(string name) {
-            if (_assemblies == null)
-                _assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            if (String.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(name))
                 return null;
-            foreach (var asm in _assemblies) {
+            foreach (var asm in Assemblies) {
                 var type = asm.GetType(name);
                 if (type != null)
                     return type;
             }
+
             // foreach (var asm in _assemblies) {
             //     var types = asm.GetTypes();
             //     var type = types.Where(t => t.FullName == name).FirstOrDefault();
@@ -26,6 +27,18 @@ namespace OneJS.Utils {
             //         return type;
             // }
             return null;
+        }
+
+        public static List<Type> FindTypesInNamespace(string namespaceName) {
+            var typesInNamespace = new List<Type>();
+            foreach (var asm in Assemblies) {
+                foreach (var type in asm.GetTypes()) {
+                    if (type.Namespace == namespaceName)
+                        typesInNamespace.Add(type);
+                }
+            }
+
+            return typesInNamespace;
         }
     }
 }
