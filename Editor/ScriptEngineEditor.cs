2b0a521b144406e6142f79665eda6af5793ac854
David <st0724@gmail.com>
2024-07-08 12:50:54 -0700

    Brought the "Open VSCode" convenience button from V2

--- Editor/ScriptEngineEditor.cs
+++ Editor/ScriptEngineEditor.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 using OneJS;
 using UnityEditor;
@@ -69,6 +72,7 @@ namespace OneJS.Editor {
         }
 
         public override void OnInspectorGUI() {
+            var scriptEngine = target as ScriptEngine;
             serializedObject.Update();
             _selectedTab = GUILayout.Toolbar(_selectedTab, new GUIContent[] {
                 new GUIContent("INTEROP", ".Net to JS Interop Settings"),
@@ -144,6 +148,10 @@ namespace OneJS.Editor {
                 default:
                     break;
             }
+            EditorGUILayout.Space(12);
+            if (GUILayout.Button(new GUIContent("Open VSCode", "Opens the Working Directory with VSCode"), GUILayout.Height(30))) {
+                VSCodeOpenDir(scriptEngine.WorkingDir);
+            }
             // EditorGUILayout.HelpBox("Hello", MessageType.None);
             // base.OnInspectorGUI();
             // EditorGUILayout.HelpBox("Hello", MessageType.None);
@@ -154,6 +162,83 @@ namespace OneJS.Editor {
             // EditorGUILayout.PropertyField(_staticClasses);
             // EditorGUILayout.PropertyField(_objects);
             serializedObject.ApplyModifiedProperties();
+        }
+
+        public static void VSCodeOpenDir(string path) {
+#if UNITY_STANDALONE_WIN
+            var processName = GetCodeExecutablePathOnWindows();
+#elif UNITY_STANDALONE_OSX || UNITY_STANDALONE_LINUX
+            var processName = GetCodeExecutablePathOnUnix();
+#else
+            var processName = "unknown";
+            UnityEngine.Debug.LogWarning("Unknown platform. Cannot open VSCode folder");
+            return;
+#endif
+            var argStr = $"\"{Path.GetFullPath(path)}\"";
+            var proc = new Process() {
+                StartInfo = new ProcessStartInfo() {
+                    FileName = processName,
+                    Arguments = argStr,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                },
+            };
+            proc.Start();
+        }
+
+        static string GetCodeExecutablePathOnWindows() {
+            string[] possiblePaths = new string[] {
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Programs\Microsoft VS Code\code.exe"),
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), @"Microsoft VS Code\code.exe"),
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Microsoft VS Code\code.exe")
+            };
+
+            foreach (var path in possiblePaths) {
+                if (File.Exists(path)) {
+                    return path;
+                }
+            }
+
+            // Additional search in PATH environment variable
+            string pathEnvironmentVariable = Environment.GetEnvironmentVariable("PATH");
+            if (pathEnvironmentVariable != null) {
+                foreach (var path in pathEnvironmentVariable.Split(Path.PathSeparator)) {
+                    string fullPath = Path.Combine(path, "code.exe");
+                    if (File.Exists(fullPath)) {
+                        return fullPath;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        static string GetCodeExecutablePathOnUnix() {
+            string[] possiblePaths = new string[] {
+                "/usr/local/bin/code",
+                "/usr/bin/code",
+                "/snap/bin/code"
+            };
+
+            foreach (var path in possiblePaths) {
+                if (File.Exists(path)) {
+                    return path;
+                }
+            }
+
+            // Additional search in PATH environment variable
+            string pathEnvironmentVariable = Environment.GetEnvironmentVariable("PATH");
+            if (pathEnvironmentVariable != null) {
+                foreach (var path in pathEnvironmentVariable.Split(Path.PathSeparator)) {
+                    string fullPath = Path.Combine(path, "code");
+                    if (File.Exists(fullPath)) {
+                        return fullPath;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
