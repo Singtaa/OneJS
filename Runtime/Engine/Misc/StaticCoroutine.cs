36d113e24ddc81b4d17a333ea048ed92c185c260
David <st0724@gmail.com>
2024-11-29 02:32:27 -0800

    added webapi example from V1

---
+++ Runtime/Engine/Misc/StaticCoroutine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace OneJS {
+    public class StaticCoroutine {
+        private class CoroutineHolder : MonoBehaviour { }
+
+        private static CoroutineHolder _holder;
+
+        private static CoroutineHolder Holder {
+            get {
+                if (_holder == null) {
+                    GameObject obj = new GameObject("StaticCoroutineHolder");
+                    _holder = obj.AddComponent<CoroutineHolder>();
+                    Object.DontDestroyOnLoad(obj);
+                }
+                return _holder;
+            }
+        }
+
+        public static void Start(IEnumerator coroutine) {
+            Holder.StartCoroutine(coroutine);
+        }
+    }
+}
---
+++ Runtime/Engine/Misc/WebApi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace OneJS {
+    public class WebApi {
+        public static void getText(string uri, Action<string> callback) {
+            StaticCoroutine.Start(GetTextCo(uri, callback));
+        }
+
+        static IEnumerator GetTextCo(string uri, Action<string> callback) {
+            using (UnityWebRequest request = UnityWebRequest.Get(uri)) {
+                yield return request.SendWebRequest();
+
+                if (request.result == UnityWebRequest.Result.ConnectionError) {
+                    callback(request.error);
+                } else {
+                    callback(request.downloadHandler.text);
+                }
+            }
+        }
+
+        public static void getImage(string uri, Action<Texture2D> callback) {
+            StaticCoroutine.Start(GetImageCo(uri, callback));
+        }
+
+        static IEnumerator GetImageCo(string uri, Action<Texture2D> callback) {
+            using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(uri)) {
+                yield return request.SendWebRequest();
+
+                if (request.result == UnityWebRequest.Result.Success) {
+                    callback(((DownloadHandlerTexture)request.downloadHandler).texture);
+                } else {
+                    Debug.Log(request.result);
+                    callback(null);
+                }
+            }
+        }
+    }
+}
