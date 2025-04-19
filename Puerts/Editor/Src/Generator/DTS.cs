1ec2b3e6b6623944a7b5cf47ee0daf0aaa9133e5
David <st0724@gmail.com>
2025-04-18 19:19:10 -0700

    Upgrade to PuerTS v2.2.0

--- Puerts/Editor/Src/Generator/DTS.cs
+++ Puerts/Editor/Src/Generator/DTS.cs
@@ -234,8 +234,8 @@ namespace Puerts.Editor
                 }
             }
 
-            public class TsTypeGenInfo {
-                public Type CSharpType; // MODDED
+            public class TsTypeGenInfo
+            {
                 public string Name;
                 public string Document;
                 public TsMethodGenInfo[] Methods;
@@ -268,7 +268,6 @@ namespace Puerts.Editor
                 {
                     var result = new TsTypeGenInfo()
                     {
-                        CSharpType = type, // MODDED
                         Name = type.Name.Replace('`', '$'),
                         Document = DocResolver.GetTsDocument(type),
                         Methods = genTypeSet.Contains(type) ? TsMethodGenInfo.FromType(type, genTypeSet) : new TsMethodGenInfo[] { },
