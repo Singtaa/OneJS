d8db43dd9b2dfcd3e8b8d48afa754230af2f107f
David <st0724@gmail.com>
2025-07-31 01:23:55 -0700

    ScriptEngine WebGL

--- Runtime/Engine/ScriptEngine.cs
+++ Runtime/Engine/ScriptEngine.cs
@@ -162,12 +162,17 @@ namespace OneJS {
             if (_jsEnv != null) {
                 _jsEnv.Dispose();
             }
-
+            
+            #if UNITY_WEBGL
+            _jsEnv = Puerts.WebGL.MainEnv.Get();
+            _jsEnv.Eval("globalThis.ONEJS_WEBGL = true;");
+            #else
             if (debuggerSupport) {
                 _jsEnv = new JsEnv(new DefaultLoader(Path.Combine(WorkingDir, basePath)), port);
             } else {
                 _jsEnv = new JsEnv();
             }
+            #endif
 
             // Some default UsingActions here. Please use OnPreInit to add more if needed (in your own code).
             _jsEnv.UsingAction<Action>();
