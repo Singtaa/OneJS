59d3882abe74ce5c599861617f4b76cbfad606d2
David <st0724@gmail.com>
2025-11-16 23:33:28 -0800

    removed unused `using` directive that's causing build issues

--- Runtime/CustomStyleSheets/unity-6.3+/StyleSheetImportErrors.cs
+++ Runtime/CustomStyleSheets/unity-6.3+/StyleSheetImportErrors.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.UIElements.StyleSheets;
 using UnityEngine;
 
 namespace OneJS.CustomStyleSheets {
